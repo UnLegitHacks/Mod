4d72b3ffb1e1a5217cff217bf861f8f82a85d09d
Some1whosAnonymous <appe27j@gmail.com>
2024-08-05 22:09:29 -0700

    Begin the custom main menu

--- src/main/java/io/unlegit/gui/UnTitleScreen.java
+++ src/main/java/io/unlegit/gui/UnTitleScreen.java
@@ -1,9 +1,13 @@
 package io.unlegit.gui;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
+// TODO: Implement this screen.
 public class UnTitleScreen extends Screen
 {
     public UnTitleScreen()
@@ -11,10 +15,21 @@ public class UnTitleScreen extends Screen
         super(Component.translatable("narrator.screen.title"));
     }
     
-    public void render(GuiGraphics guiGraphics, int width, int height, float partialTicks)
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks)
     {
-        this.renderPanorama(guiGraphics, partialTicks);
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
     }
     
-    public void renderBackground(GuiGraphics guiGraphics, int width, int height, float partialTicks) {}
+    public void renderBackground(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks)
+    {
+        ResourceLocation background = ResourceLocation.fromNamespaceAndPath("unlegit", "background.png");
+        int bW = 853, bH = 398;
+        
+        // If they're too small, make them bigger while maintaining the aspect ratio.
+        while (bW < width || bH < height) { bW++; bH++; } 
+        
+        GlStateManager._enableBlend();
+        guiGraphics.setColor(1, 1, 1, 1);
+        guiGraphics.blit(background, -mouseX / 6, 0, bW, bH, bW, bH, bW, bH);
+    }
 }
